ad490c0f17a3de10b2dad72f88f411e628f3c22b
shirantha97 <39570216+shirantha97@users.noreply.github.com>
2020-12-01 10:25:11 +0530

    basic charts implementation

--- src/app/results/cardiovascular/cardiovascular.page.ts
+++ src/app/results/cardiovascular/cardiovascular.page.ts
@@ -1,40 +1,89 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import {LoaderService} from '../../loader-service.service'
-import {UsersService} from "../../users.service";
-
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { LoaderService } from "../../loader-service.service";
+import { UsersService } from "../../users.service";
+import { Chart } from "chart.js";
 
 @Component({
-  selector: 'app-cardiovascular',
-  templateUrl: './cardiovascular.page.html',
-  styleUrls: ['./cardiovascular.page.scss'],
+  selector: "app-cardiovascular",
+  templateUrl: "./cardiovascular.page.html",
+  styleUrls: ["./cardiovascular.page.scss"],
 })
 export class CardiovascularPage implements OnInit {
+  query_params: any;
+  bmisuggestion: any;
+  ishidden: boolean = true;
 
-  query_params : any
-  bmisuggestion : any
-  ishidden : boolean = true
+  labelLen: Array<number> = [];
+
+  @ViewChild("barChart") barChart;
+  bars: any;
+  colorArray: any;
 
   constructor(
-    public activatedRoute : ActivatedRoute, 
+    public activatedRoute: ActivatedRoute,
     public Loader: LoaderService,
     private user: UsersService
   ) {
-    this.activatedRoute.queryParams.subscribe((res)=>{
-      this.query_params = JSON.parse(res.value)
-      console.log(this.query_params)
-      if(this.query_params.ob_stage == "1"){
-        this.checkObstage()
-        this.ishidden = false
+    this.activatedRoute.queryParams.subscribe((res) => {
+      this.query_params = JSON.parse(res.value);
+      console.log(this.query_params);
+      if (this.query_params.ob_stage == "1") {
+        this.checkObstage();
+        this.ishidden = false;
       }
     });
-   }
-
-  ngOnInit() {
   }
 
-  checkObstage(){
-    this.bmisuggestion = "Maintain the calory intake lower than the TDEE estimation"
+  ionViewDidEnter() {
+    this.createBarChart();
   }
 
+  createBarChart() {
+    let lenobj = 1;
+    this.query_params.ob_stage_levels.forEach((element) => {
+      this.labelLen.push(lenobj++);
+    });
+    console.log(this.query_params.ob_stage_levels);
+    this.bars = new Chart(this.barChart.nativeElement, {
+      type: "line",
+      data: {
+        labels: this.labelLen,
+        datasets: [
+          {
+            label: "Obesity Stage",
+            data: this.query_params.ob_stage_levels,
+            backgroundColor: "rgba(0,0,0,0)",
+            borderColor: "rgb(38, 194, 129)",
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+          xAxes: [
+            {
+              barPercentage: 0.9,
+            },
+          ],
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    });
+  }
+
+  ngOnInit() {}
+
+  checkObstage() {
+    this.bmisuggestion =
+      "Maintain the calory intake lower than the TDEE estimation";
+  }
 }
